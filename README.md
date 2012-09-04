a5769c9ef960e83c0679615f7686982447a63008
Trey Shugart <treshugart@gmail.com>
2012-09-04 19:00:12 +1000

    Finalize API. Update readme.

--- README.md
+++ README.md
@@ -1,4 +1,71 @@
-knockup
-=======
+Knockup - ORM for KnockoutJS
+============================
 
-ORM for KnockoutJS.
+Knockup gives you a simple API for managing and relating JavaScript objects and handing them off to Knockout to use.
+
+Knockout is awesome. What it is designed to do, it does almost flawlessly. However, I've found that no matter what, I've yearned for some form of object management and automated observers. Thus, Knockup was born.
+
+Overview
+--------
+
+Knockup does not change anything about how you compose your views, just how you compose your models.
+
+    // define your person model
+    var person = ku.model({
+        forename: '',
+        surname: '',
+        selected: false,
+        getName: function() {
+            return this.forename() + ' ' + this.surname();
+        },
+        setName: function(name) {
+            var names = name.split(' ');
+            this.forename(names[0]);
+            this.surname(names[1]);
+        },
+        toggle: function() {
+            this.selected(!this.selected());
+        }
+    });
+
+    // define the app model
+    var app = ku.model({
+        people: person.collection,
+        getPerson: function() {
+            return this.people().findOne({ selected: true });
+        }
+    });
+    
+    // bind to the view
+    app.knockup();
+    
+    // update the DOM
+    app.person().name('John Doe');
+
+Your view may looks something like:
+
+    <div data-bind="text: person().forename"></div>
+    <ul data-bind="foreach: people">
+        <li><a href="#" data-bind="click: toggle, text: name"></a></li>
+    </ul>
+
+Conventions
+-----------
+
+Knockup makes a few design choices for you, but keeps things as simple as possible:
+
+1. You define your model by passing an object to the `ku.model` function and it returns a constructor for that model.
+2. Any value passed in become observable.
+3. Functions prefixed with `get` or `set` become computed observables. All other functions are just normal methods.
+4. If you pass in a model, that property will always contain an instance of that model.
+5. If you pass in a model collection, that property will always contain a collection of its corresponding models.
+
+In order to apply your aggregate root (for you DDD peeps) to the view, you can either:
+
+    app.knockup()
+    
+Or do it the old fasioned way:
+
+    ko.applyBindings(app);
+
+Whatever is easier for you.
--- knockup.js
+++ knockup.js
@@ -4,198 +4,41 @@
 !function() {
 
     // If knockout is not defined, let them know.
-    if (_undefined(ko)) {
-        throw new Error('Cannot find KnockoutJS. Download at https://github.com/SteveSanderson/knockout/downloads.');
+    if (typeof ko === 'undefined') {
+        throw new Error('KnockoutJS is required. Download at https://github.com/SteveSanderson/knockout.');
     }
 
-    // Expose the knockup object.
+    // Expose.
     window.ku = {};
-
-    // Knockup sets are collections of models.
-    // The model is just a constructor returned from ku.model().
-    // The data is used to give the array an initialized state.
-    ku.set = function(model, data) {
-        // The set data is just an observable array.
-        var _data = ko.observableArray();
-
-        // Returns the raw observable array.
-        this.ko = function() {
-            return _data();
-        };
-
-        // Returns the item at the specified index.
-        this.at = function(index) {
-            if (!this.has(index)) {
-                this.insert(index, {});
-            }
-            return _data()[index];
-        };
-
-        // Returns whether or not an item exists at the specified index.
-        this.has = function(index) {
-            return _defined(_data(), index);
-        };
-
-        // Prepends the specified model.
-        this.prepend = function(item) {
-            return this.insert(0, item);
-        };
-
-        // Appends the specified model.
-        this.append = function(item) {
-            return this.insert(_data().length, item);
-        };
-
-        // Inserts the model at the specified index.
-        this.insert = function(at, item) {
-            _data.splice(at, 0, new model(item));
-            return this;
-        };
-
-        // Fills the set with the specified data.
-        this.fill = function(data) {
-            _each(data, this, function(i, item, set) {
-                set.append(item);
-            });
-            return this;
-        };
-
-        // Returns the total nubmer of items in the set.
-        this.count = function() {
-            return _data().length;
-        };
-
-        // Finds several items in the set.
-        this.find = function(query, limit) {
-            var found = [];
-
-            _each(_data(), found, function(i, model) {
-                if (model.is(query)) {
-                    found.push(model);
-                }
-
-                if (limit && found.length === limit) {
-                    return;
-                }
-            });
-
-            return new ku.set(model, found);
-        };
-
-        // Finds one item in the set.
-        this.findOne = function(query) {
-            var set = this.find(query, 1);
-            if (set.count()) {
-                return set.at(0);
-            }
-        };
-
-        // Fill with the initial data.
-        this.fill(data);
-    };
 
     // Creates a knockup model.
     // The initial data in the model is specified by the "inherit" argument.
-    ku.model = function(inherit) {
-        var _allow  = [],
-            _block  = [],
-            _many   = {},
-            _one    = {},
-            _strict = false;
+    ku.model = function(define) {
+        var model = function(data) {
+            var comp = {},
+            self = this;
 
-        // This is the constructor that gets modified and returned.
-        var ret = function(fill) {
-            var _data = {},
-                _self = this;
-
-            // Returns a knockout observable, observableArray or computed.
-            this.ko = function(name) {
-                // property must be accessible
-                if (!this.access(name)) {
-                    return;
-                }
-
-                // has-manys handle their own observers
-                if (_defined(_many, name)) {
-                    return _data[name]().ko();
-                }
-
-                // allow for flexible objects
-                if (_undefined(_data, name)) {
-                    _data[name] = ko.observable();
-                }
-
-                // return the property observer
-                return _data[name];
-            };
-
-            // Returns the specified value.
-            this.get = function(name) {
-                var obs = this.ko(name);
-
-                // if no observer is returned, we don't have access
-                if (!obs) {
-                    return;
-                }
-
-                // has-manys handle their own observers
-                if (_defined(_many, name)) {
-                    return _data[name]();
-                }
-
-                return obs();
-            };
-
-            // Sets the specified value.
-            this.set = function(name, value) {
-                var obs = this.ko(name);
-
-                // if no observer is returned, we don't have access
-                if (!obs) {
+            // The observer is what is returned when the object is accessed.
+            this.observer = ko.computed({
+                read: function() {
                     return this;
-                }
-
-                // has-manys handle their own observers
-                if (_defined(_one, name) || _defined(_many, name)) {
-                    _data[name]().fill(value);
-                } else if (obs) {
-                    obs(value);
-                }
-
-                return this;
-            };
-
-            // Returns whether or not the model has the specified value.
-            this.has = function(name) {
-                return _defined(_data, name);
-            };
-
-            // Removes the specified value.
-            this.remove = function(name) {
-                if (this.has(name)) {
-                    delete _data[name];
-                }
-                return this;
-            };
-
-            // Returns whether or not the specified property is accessible.
-            this.access = function(name) {
-                if (_in(_block, name)) {
-                    return true;
-                }
-
-                if (_in(_allow, name)) {
-                    return false;
-                }
-
-                return !_strict;
-            };
+                },
+                write: function(value) {
+                    this.fill(value);
+                },
+                owner: this
+            });
 
             // Fills the model with the specified values.
             this.fill = function(obj) {
-                _each(obj, function(name, value) {
-                    _self.set(name, value);
+                // Update each value.
+                each(obj, function(name, value) {
+                    self[name](value);
                 });
+
+                // Tell everyone.
+                this.observer.notifySubscribers();
+
                 return this;
             };
 
@@ -203,8 +46,8 @@
             this.is = function(query) {
                 var found = false;
 
-                _each(query, function(name, value) {
-                    if (_self.has(name) && _self.get(name) === value) {
+                each(query, function(name, value) {
+                    if (typeof this[name] === 'function' && this[name]() === value) {
                         found = true;
                     } else {
                         found = false;
@@ -222,102 +65,171 @@
                 return this;
             };
 
-            // Apply computed observable methods.
-            _each(this, [_data, this], function(name, value, data) {
-                if (!_function(value)) {
+            // Define the object structure.
+            each(define, function(i, v) {
+                if (ku.isModel(v) || ku.isCollection(v)) {
+                    self[i] = new v().observer;
                     return;
                 }
 
-                var matched = name.match(/^(get|set)([A-Z][a-zA-Z0-9]?+)$/);
+                // Handle functions, both get*, set* and normal.
+                if (typeof v === 'function') {
+                    var type;
 
-                if (matched) {
-                    data[0][_lcfirst(matched[2])] = ko.computed(value, data[1]);
+                    if (i.indexOf('get') === 0) {
+                        type = 'read';
+                    } else if (i.indexOf('set') === 0) {
+                        type = 'write';
+                    }
+
+                    // If a "get" or "set" prefix was found, it is a computed observable.
+                    if (type) {
+                        var name = i.substring(3, 4).toLowerCase() + i.substring(4);
+
+                        if (typeof comp[name] === 'undefined') {
+                            comp[name] = {};
+                        }
+
+                        comp[name][type] = v;
+
+                        return;
+                    }
+
+                    self[i] = v;
+
+                    return;
                 }
+
+                self[i] = ko.observable(v);
             });
 
-            // Apply has-one relationships.
-            _each(_one, _data, function(name, value, data) {
-                data[name] = ko.observable(new value);
+            // Apply computed properties.
+            each(comp, function(name, comp) {
+                comp.owner  = self;
+                self[name] = ko.computed(comp);
             });
-
-            // Apply has-many relationships.
-            _each(_many, _data, function(name, value, data) {
-                data[name] = ko.observable(new ku.set(value));
-            });
-
-            // Fill with default values.
-            this.fill(inherit);
 
             // Fill with instance values.
-            this.fill(fill);
+            this.fill(data);
         };
 
-        // Creates a new model using the specified properties.
-        ret.create = function(props) {
-            return new ret().fill(props);
-        };
+        // The set constuctor for the current model.
+        model.collection = ku.collection(model);
 
-        // Applies a one-to-one relationship.
-        ret.one = function(name, model) {
-            var models = name;
+        return model;
+    };
 
-            if (typeof name === 'string') {
-                models = {};
-                models[name] = model;
-            }
+    // Contains a set of models and can be used like an array.
+    ku.collection = function(model) {
+        return function(data) {
+            Array.prototype.push.apply(this, []);
 
-            _each(models, _one, function(name, model, one) {
-                one[name] = model;
+            // The observer is what is returned when the object is accessed.
+            this.observer = ko.computed({
+                read: function() {
+                    return this;
+                },
+                write: function(value) {
+                    this.fill(value);
+                },
+                owner: this
             });
 
-            return ret;
+            // Returns the item at the specified index.
+            this.at = function(index) {
+                if (!this.has(index)) {
+                    this.insert(index, {});
+                }
+
+                return this[index];
+            };
+
+            // Returns whether or not an item exists at the specified index.
+            this.has = function(index) {
+                return typeof this[index] !== 'undefined';
+            };
+
+            // Prepends the specified model.
+            this.prepend = function(item) {
+                return this.insert(0, item);
+            };
+
+            // Appends the specified model.
+            this.append = function(item) {
+                return this.insert(this.length, item);
+            };
+
+            // Inserts the model at the specified index.
+            this.insert = function(at, item) {
+                Array.prototype.splice.call(this, at, 0, new model(item));
+                this.observer.notifySubscribers();
+                return this;
+            };
+
+            // Fills the set with the specified data.
+            this.fill = function(data) {
+                var self = this;
+
+                each(data, function(i, model) {
+                    self.append(model);
+                });
+
+                return this;
+            };
+
+            // Executes the callback for each item in the set.
+            this.each = function(fn, data) {
+                for (var i = 0; i < this.length; i++) {
+                    fn(i, this[i]);
+                }
+                return this;
+            };
+
+            // Finds several items in the set.
+            this.find = function(query, limit) {
+                var found = [];
+
+                this.each(function(i, model) {
+                    if (model.is(query)) {
+                        found.push(model);
+                    }
+
+                    if (limit && found.length === limit) {
+                        return;
+                    }
+                });
+
+                return new ku.set(model, found);
+            };
+
+            // Finds one item in the set.
+            this.findOne = function(query) {
+                var set = this.find(query, 1);
+
+                if (set.count()) {
+                    return set.at(0);
+                }
+            };
+
+            // Fill with the initial data.
+            this.fill(data);
         };
+    };
 
-        // Applies a one-to-many relationship.
-        ret.many = function(name, model) {
-            var models = name;
+    // Returns whether or not the speicfied function is a model constructor.
+    var modelString = ku.model().toString();
+    ku.isModel = function(fn) {
+        return fn.toString() === modelString;
+    };
 
-            if (typeof name === 'string') {
-                models = {};
-                models[name] = model;
-            }
-
-            _each(models, _many, function(name, model, many) {
-                many[name] = model;
-            });
-
-            return ret;
-        };
-
-        // Allows the specified items as model properties.
-        ret.allow = function(list) {
-            _each(list, _block, function(i, v, l) {
-                l.push(v);
-            });
-            return ret;
-        };
-
-        // Blocks the specified model properties from being accessed.
-        ret.block = function(list) {
-            _each(list, _allow, function(i, v, l) {
-                l.push(v);
-            });
-            return ret;
-        };
-
-        // Makes the model closed off from arbitrary modifications.
-        // By default, models will accept any property.
-        ret.strict = function(bool) {
-            _strict = bool ? true : false;
-            return ret;
-        };
-
-        // Return the model constructor.
-        return ret;
+    // Returns whether or not the speicfied function is a collection constructor.
+    var collectionString = ku.collection().toString();
+    ku.isCollection = function(fn) {
+        return fn.toString() === collectionString;
     };
 
     // Iterates over an array or hash.
-    function _each(items, data, fn) {
+    function each(items, data, fn) {
         var fn    = arguments.length === 2 ? data : fn;
         var items = items || [];
 
@@ -335,30 +247,5 @@
             }
         }
     };
-
-    // Returns whether or not the object or specified property on the object is defined.
-    function _defined(obj, prop) {
-        return !_undefined(obj, prop);
-    }
-
-    // Returns whether or not the object or function on the object is a function.
-    function _function(obj, fn) {
-        typeof arguments.length ? typeof obj[fn] === 'function' : typeof obj === 'function';
-    }
-
-    // Return whether or not the specified value exists in the given array.
-    function _in(arr, val) {
-        return arr.indexOf(val) > -1;
-    }
-
-    // Makes the first character of the given string uppercase and returns the string.
-    function _ucfirst(str) {
-        return str.charAt(0).toUpperCase() + str.substr(1);
-    }
-
-    // Returns whether or not the object or specified property on the object is undefined.
-    function _undefined(obj, prop) {
-        return arguments.length === 2 ? typeof obj[prop] === 'undefined' : typeof obj === 'undefined';
-    }
 
 }();
