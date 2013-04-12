9885f5496c56bcd5612ca5418c059661da744df1
Russell Holman <russholio@gmail.com>
2013-04-12 17:14:11 +1000

    MS-188 Stopped Model.from() from trying to clone computed properties.

--- src/ku/model.js
+++ src/ku/model.js
@@ -10,7 +10,13 @@ ku.model = function(definition) {
 
         this.from = function(obj) {
             if (ku.isModel(obj)) {
-                obj = obj.raw();
+                var data = obj.raw();
+
+                each(obj.$self.computed, function(name) {
+                    delete data[name];
+                });
+
+                obj = data;
             }
 
             each(obj, function(name, value) {
